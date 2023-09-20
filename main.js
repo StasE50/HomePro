1d0518597e194a4cd82255f82b9a1673fe58c8ae
Stas Zolotar <staszolotar57@gmail.com>
2023-09-20 14:22:37 +0300

    end burger menu

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const burgerBtn = document.querySelector('.header__burger-btn');
 var links =document.querySelectorAll('.nav_ref');
 const menu = document.querySelector('.menu');
 const overlay=document.querySelector('.overlay');
+const logo = document.querySelector('.logo');
+const servicesTime = document.querySelector('.services_time');
 // Обработчик события клика на бургере
 burgerBtn.addEventListener('click', function() {
 	menu.classList.toggle('active');
   burgerBtn.classList.toggle('active');
 overlay.classList.toggle('active');
   document.body.classList.toggle('menu-open');
+ 
 });
 overlay.addEventListener('click',()=>{
   menu.classList.toggle('active');
@@ -18,9 +21,14 @@ document.body.classList.toggle('menu-open');
 });
 links.forEach(function(link) {
   link.addEventListener('click', function() {
-    menu.classList.toggle('active'); // Закрываем меню
+   if(window.innerWidth<=780){
+overlay.classList.toggle('active');
+menu.classList.toggle('active'); // Закрываем меню
     burgerBtn.classList.toggle('active'); // Меняем состояние бургера
     document.body.classList.toggle('menu-open');
+   }
+  
+    
   });
 });
 
